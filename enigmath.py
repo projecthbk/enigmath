dbaad42ee16e18d463a650e79b38d1b15cbcbf8b
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 19:26:46 +0100

    Update enigmath.py

--- enigmath.py
+++ enigmath.py
@@ -2,24 +2,24 @@
 """Author: Krzysztof Hrybacz <krzysztof@zygtech.pl>"""
 """License: GNU General Public License -- version 3"""
 
-import sys, os
+import sys
 from math import *
 
 def main():
-    if (len(sys.argv)!=3):
+    if len(sys.argv)!=3:
         print("MATH ENIGMA ENCODER: Create your own any \"enrypting machine\".\nUsage: enigmath.py <filename> <math formula in python format>\nExample: enigmath.py Archive.zip \"(pi+cos(#*30))*100/sqrt(3)\"")
         quit()
     filename = sys.argv[1]
     formula = sys.argv[2]
-    if (formula.count('#')==0):
+    if formula.count('#')==0:
         print("MATH ENIGMA ENCODER: Create your own any \"enrypting machine\".\nUse variable # as iterator at least once in formula!")
         quit()
-    if (formula.count(' ')!=0 or formula.count('\n')!=0):
+    if formula.count(' ')!=0 or formula.count('\n')!=0:
         print("MATH ENIGMA ENCODER: Create your own any \"enrypting machine\".\nDon't use spaces or new line characters in formula!")
         quit()
     formula = formula.replace('#','iterator')
     source = open(filename,'rb')
-    if (filename[-9:]!='.enigmath'):
+    if filename[-9:]!='.enigmath':
         destination = open(filename + '.enigmath','wb')
     else:
         destination = open(filename[:-9] + '.original','wb')
@@ -27,9 +27,9 @@ def main():
     result = 0
     n = 1
     byte = ''
-    for i in range (0,os.path.getsize(filename)):
+    while True:
         for bytepos in range (0,7):
-            if (result==0 or index > len(bin(result)[2:])): 
+            if result==0 or index > len(bin(result)[2:]): 
                 iterator = n
                 result = round(eval(formula)) 
                 index = 1
@@ -38,9 +38,12 @@ def main():
             byte = str(byte) + bitresult[bytepos]
             index = index + 1
         cbyte = int(byte,2)
-        sbyte = int.from_bytes(source.read(1), byteorder='big')
+        rbyte = source.read(1)
+        if not rbyte:
+            break
+        sbyte = int.from_bytes(rbyte,byteorder='big')
         ebyte = sbyte ^ cbyte
-        destination.write(ebyte.to_bytes(1, byteorder='big'))
+        destination.write(ebyte.to_bytes(1,byteorder='big'))
         byte = ''
     source.close()
     destination.close()
