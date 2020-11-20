996b9463dbb447801f1588124dfbc516ff2d282b
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-20 14:18:08 +0100

    Update enigmathizer.py

--- enigmathizer.py
+++ enigmathizer.py
@@ -6,16 +6,16 @@ import sys,os,hashlib
 
 def main():
     if len(sys.argv)!=4:
-        print("ENIGMATHIZER: Creates enigmath protected ZIP package for large files.\nUsage: enigmathize.py <outputzipname without extension> <file, foldername or wildcard> <math formula in python format>\nExample: enigmathize.py MyZipFile /MyFolder \"(pi+cos(#*30))*100/sqrt(3)\"")
+        print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nUsage: enigmathize.py <outputzipname> <file or folder name> <formula>\nExample: enigmathize.py MyZipFile /MyFolder \"(pi+cos(#*30))*100/sqrt(3)\"")
         quit()
     zipname = sys.argv[1]
     foldername = sys.argv[2]
     formula = sys.argv[3]
     if formula.count('#')==0:
-        print("ENIGMATHIZER: Creates enigmath protected ZIP package for large files.\nUse variable # as iterator at least once in formula!")
+        print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nUse variable # as iterator at least once in formula!")
         quit()
     if formula.count(' ')!=0 or formula.count('\n')!=0:
-        print("ENIGMATHIZER: Creates enigmath protected ZIP package for large files.\nDon't use spaces or new line characters in formula!")
+        print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nDon't use spaces or new line characters in formula!")
         quit()
     password = hashlib.sha256(os.urandom(64)).hexdigest()
     os.system("zip -P " + password + " -r " + zipname + ".zip \"" + foldername + "\"")
