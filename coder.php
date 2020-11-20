f84d9e0af0e8e4ea151a607a7af973b77043f0f4
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-20 10:37:32 +0100

    Update coder.php

--- coder.php
+++ coder.php
@@ -1,7 +1,7 @@
 <?php
 		ini_set('max_execution_time', 0);
 		if ($_FILES['encfile']['size']>1048576) {
-			echo '<meta http-equiv="refresh" content="3;url=./?formula=' . urlencode($_POST['formula']) . '" />MAX FILESIZE: 1024kB';
+			echo '<meta http-equiv="refresh" content="3;url=./?formula=' . urlencode($_POST['formula']) . '" /><center>MAX FILESIZE: 1024kB</center>';
 			exit();
 		}
 		$filename=basename($_FILES['encfile']['name']);
@@ -15,7 +15,7 @@
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		$filename = 'tmp' + rand(1000,9999)
+		$filename = 'tmp' + strval(rand(1000,9999));
 		move_uploaded_file($_FILES['encfile']['tmp_name'],$filename);
 		shell_exec('python3 enigmath.py ' . $filename . ' "' . $_POST['formula'] . '"');
 		while (filesize($filename . '.enigmath')<$_FILES['encfile']['size']) sleep(1);
