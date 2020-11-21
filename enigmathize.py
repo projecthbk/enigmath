9b0d9f8e0bc8f35a8374b55fd1f18161b6168d9d
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-22 00:58:45 +0100

    Update enigmathize.py

--- enigmathize.py
+++ enigmathize.py
@@ -5,6 +5,9 @@
 import sys,os,shutil,hashlib
 
 def main():
+    if os.path.isfile('.lock'):
+        print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nCan't process packing/unpacking because of \"lock\" file being present.\nIf you're sure all tasks are finished remove this file and try again.")
+        quit()		
     open('.lock','a').close()
     if len(sys.argv)!=4 and len(sys.argv)!=3:
         print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nUsage: enigmathize.py <outputzipname> <file or folder name> <formula>\nTo decrypt ZIP file: enigmathize.py <enigmathizer zip file> <formula>\nExample: enigmathize.py ZipName /Folders \"(pi+cos(#*30))*100/sqrt(3)\"")
@@ -38,13 +41,13 @@ def main():
         os.remove(zipname + ".zip")
     if len(sys.argv)==3:
         os.system("unzip \"" + unpackname + "\"")
-        os.system("python3 " + os.path.abspath(os.path.dirname(sys.argv[0])) + "/enigmath.py password.enigmath \"" + formula + "\"")
-        if not os.path.isfile("password.original"):
-            print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nWrong formula! Assure that it's formula that ZIP was encrypted with.")
-            quit()        
+        os.system("python3 " + os.path.abspath(os.path.dirname(sys.argv[0])) + "/enigmath.py password.enigmath \"" + formula + "\"")     
         passfile = open("password.original","rt")
         password = passfile.read()
         passfile.close()
+        if password=="":
+            print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nWrong formula! Assure that it's formula that ZIP was encrypted with.")
+            quit()           
         os.remove("password.enigmath")
         os.remove("password.original")   
         os.system("unzip -P " + password + " \"" + unpackname[:-13] + ".zip\" -d \"./" + unpackname[:-13] + "\"")
