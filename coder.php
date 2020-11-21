82d5a31c974c4f913c8a85864cbedfa97d4ac176
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-21 22:59:15 +0100

    Update coder.php

--- coder.php
+++ coder.php
@@ -6,15 +6,18 @@
 		}
 		if ($_FILES['encfile']['size']>1048576) {
 			if (substr($filename,-13)=='.enigmath.zip') {
-				echo '<meta http-equiv="refresh" content="3;url=./?formula=' . urlencode($_POST['formula']) . '" /><center>ENIGMATH: To unpack "enigmath.zip" file open archive and decode "password.enigmath".</center>';
-				exit();
-			} else
+				$file = explode('.',$filename)[0] . '.zip';
+				$type = 'unpack';
+			} else {
 				$file = explode('.',$filename)[0] . '.enigmath.zip';
+				$type = 'pack';
+			}
 		} else {
 			if (substr($filename,-9)=='.enigmath')
 				$file = substr($filename,0,strlen($filename)-9) . '.original';
 			else
 				$file = $filename . '.enigmath';
+			$type = 'encode';
 		}
 		header('Content-Description: EnigMATH Encoded File');
 		header('Content-Type: application/octet-stream');
@@ -22,20 +25,30 @@
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		$filename = 'tmp' . rand(1000,9999);
-		move_uploaded_file($_FILES['encfile']['tmp_name'],$filename);
-		if ($_FILES['encfile']['size']>1048576) {
+		if ($type=='pack') {
+			$filename = basename($_FILES['encfile']['name']);
+			move_uploaded_file($_FILES['encfile']['tmp_name'],$filename);
 			shell_exec('python3 enigmathize.py ' . explode('.',$file)[0] . ' ' . $filename . ' "' . $_POST['formula'] . '"');
 			while (filesize(explode('.',$file)[0] . '.enigmath.zip')<=filesize(explode('.',$file)[0] . '.zip')) sleep(1);
 			readfile(explode('.',$file)[0] . '.enigmath.zip');
 			unlink($filename);
 			unlink(explode('.',$file)[0] . '.enigmath.zip');
-		}
-		else {
+		} elseif ($type=='unpack') {
+			$filename = basename($_FILES['encfile']['name']);
+			move_uploaded_file($_FILES['encfile']['tmp_name'],$filename);
+			shell_exec('python3 enigmathize.py ' . $filename . ' "' . $_POST['formula'] . '"');
+			sleep(10);
+			while (file_exists('password.enigmath')) sleep(1);
+			readfile(substr($filename,0,strlen($filename)-13) . '.zip');
+			unlink($filename);
+			unlink(substr($filename,0,strlen($filename)-13) . '.zip');
+		} elseif ($type=='encode') {
+			$filename = 'tmp' . rand(1000,9999);
+			move_uploaded_file($_FILES['encfile']['tmp_name'],$filename);
 			shell_exec('python3 enigmath.py ' . $filename . ' "' . $_POST['formula'] . '"');
 			while (filesize($filename . '.enigmath')<$_FILES['encfile']['size']) sleep(1);
 			readfile($filename . '.enigmath');
 			unlink($filename);
 			unlink($filename . '.enigmath');
-		}		
+		} 
 ?>
