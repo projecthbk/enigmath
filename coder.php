e2557a802da6f37e77528d8778a3d71f2ddc92b7
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-21 20:47:37 +0100

    Update coder.php

--- coder.php
+++ coder.php
@@ -1,14 +1,21 @@
 <?php
-		ini_set('max_execution_time', 0);
-		if ($_FILES['encfile']['size']>1048576) {
-			echo '<meta http-equiv="refresh" content="3;url=./?formula=' . urlencode($_POST['formula']) . '" /><center>MAX FILESIZE: 1024kB</center>';
+		$filename=basename($_FILES['encfile']['name']);
+		if ($filename=='') {
+			echo '<meta http-equiv="refresh" content="3;url=./?formula=' . urlencode($_POST['formula']) . '" /><center>ENIGMATH: File not uploaded properly. Allowed size up to 1GB.</center>';
 			exit();
 		}
-		$filename=basename($_FILES['encfile']['name']);
-		if (substr($filename,-9)=='.enigmath')
-			$file = substr($filename,0,strlen($filename)-9) . '.original';
-		else
-			$file = $filename . '.enigmath';
+		if ($_FILES['encfile']['size']>1048576) {
+			if (substr($filename,-13)=='.enigmath.zip') {
+				echo '<meta http-equiv="refresh" content="3;url=./?formula=' . urlencode($_POST['formula']) . '" /><center>ENIGMATH: To unpack "enigmath.zip" file open archive and decode "password.enigmath".</center>';
+				exit();
+			} else
+				$file = explode('.',$filename)[0] . '.enigmath.zip';
+		} else {
+			if (substr($filename,-9)=='.enigmath')
+				$file = substr($filename,0,strlen($filename)-9) . '.original';
+			else
+				$file = $filename . '.enigmath';
+		}
 		header('Content-Description: EnigMATH Encoded File');
 		header('Content-Type: application/octet-stream');
 		header('Content-Disposition: attachment; filename="' . $file . '"');
@@ -17,8 +24,18 @@
 		header('Pragma: public');
 		$filename = 'tmp' . rand(1000,9999);
 		move_uploaded_file($_FILES['encfile']['tmp_name'],$filename);
-		shell_exec('python3 enigmath.py ' . $filename . ' "' . $_POST['formula'] . '"');
-		while (filesize($filename . '.enigmath')<$_FILES['encfile']['size']) sleep(1);
-		readfile($filename . '.enigmath');
-		foreach(glob($filename . '*') as $f) unlink($f);
+		if ($_FILES['encfile']['size']>1048576) {
+			shell_exec('python3 enigmathize.py ' . explode('.',$file)[0] . ' ' . $filename . ' "' . $_POST['formula'] . '"');
+			while (filesize(explode('.',$file)[0] . '.enigmath.zip')<=filesize(explode('.',$file)[0] . '.zip')) sleep(1);
+			readfile(explode('.',$file)[0] . '.enigmath.zip');
+			unlink($filename);
+			unlink(explode('.',$file)[0] . '.enigmath.zip');
+		}
+		else {
+			shell_exec('python3 enigmath.py ' . $filename . ' "' . $_POST['formula'] . '"');
+			while (filesize($filename . '.enigmath')<$_FILES['encfile']['size']) sleep(1);
+			readfile($filename . '.enigmath');
+			unlink($filename);
+			unlink($filename . '.enigmath');
+		}		
 ?>
