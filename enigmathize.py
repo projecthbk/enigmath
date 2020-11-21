72beea812221ecd95bf7ecf4363714f254303bb1
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-22 00:06:44 +0100

    Update enigmathize.py

--- enigmathize.py
+++ enigmathize.py
@@ -5,8 +5,9 @@
 import sys,os,shutil,hashlib
 
 def main():
+    open('.lock','a').close()
     if len(sys.argv)!=4 and len(sys.argv)!=3:
-        print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nUsage: enigmathize.py <outputzipname> <file or folder name> <formula>\nOr to decrypt: enigmathize.py <enigmathizer zip file> <formula>\nExample: enigmathize.py ZipName /Folders \"(pi+cos(#*30))*100/sqrt(3)\"")
+        print("ENIGMATHIZER: Creates EnigMATH protected ZIP package for large files.\nUsage: enigmathize.py <outputzipname> <file or folder name> <formula>\nTo decrypt ZIP file: enigmathize.py <enigmathizer zip file> <formula>\nExample: enigmathize.py ZipName /Folders \"(pi+cos(#*30))*100/sqrt(3)\"")
         quit()
     if len(sys.argv)==4:
         zipname = sys.argv[1]
@@ -26,27 +27,28 @@ def main():
         quit()
     if len(sys.argv)==4:
         password = hashlib.sha256(os.urandom(256)).hexdigest()
-        os.system("zip -P " + password + " -r " + zipname + ".zip \"" + foldername + "\"")
+        os.system("zip -P " + password + " -r \"" + zipname + ".zip\" \"" + foldername + "\"")
         passfile = open("password","wt")
         passfile.write(password)
         passfile.close()
         os.system("python3 " + os.path.abspath(os.path.dirname(sys.argv[0])) + "/enigmath.py password \"" + formula + "\"")
-        os.system("zip -r " + zipname + ".enigmath.zip " + zipname + ".zip password.enigmath")
+        os.system("zip -r \"" + zipname + ".enigmath.zip\" \"" + zipname + ".zip\" password.enigmath")
         os.remove("password")
         os.remove("password.enigmath")
         os.remove(zipname + ".zip")
     if len(sys.argv)==3:
-        os.system("unzip " + unpackname)
+        os.system("unzip \"" + unpackname + "\"")
         os.system("python3 " + os.path.abspath(os.path.dirname(sys.argv[0])) + "/enigmath.py password.enigmath \"" + formula + "\"")
         passfile = open("password.original","rt")
         password = passfile.read()
         passfile.close()
-        os.remove("password.original")   
-        os.system("unzip -P " + password + " " + unpackname[:-13] + ".zip -d ./" + unpackname[:-13])
-        os.remove(unpackname[:-13] + ".zip")
-        os.system("zip -r -j " + unpackname[:-13] + ".zip " + unpackname[:-13])
-        shutil.rmtree(unpackname[:-13])
         os.remove("password.enigmath")
+        os.remove("password.original")   
+        os.system("unzip -P " + password + " \"" + unpackname[:-13] + ".zip\" -d \"./" + unpackname[:-13] + "\"")
+        os.remove(unpackname[:-13] + ".zip")
+        os.system("zip -r -j \"" + unpackname[:-13] + ".original.zip\" \"" + unpackname[:-13] + "\"")
+        shutil.rmtree(unpackname[:-13])
+    os.remove(".lock")
         
 if __name__ == '__main__':
     main()
